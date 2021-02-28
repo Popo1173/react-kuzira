bb256a2f2f2135b3a45a58887725ed616a67780a
荒井忠輔 <mr.popo1173@gmail.com>
2021-02-28 13:00:58 +0900

    node&npm

---
+++ project_a/index.js
@@ -0,0 +1,12 @@
+const colors = require('colors');
+
+const s1 = "S1";
+const s2 = "S2";
+const s3 = "S3";
+const s4 = "S4"
+
+console.log(s1.underline.red);
+console.log(s2.inverse.blue);
+console.log(s3.rainbow);
+console.log(s4.inverse.red);
+
